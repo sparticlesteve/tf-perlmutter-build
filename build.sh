98840d671be2d00938f39d8d014075fc1b5cbae1
Steve Farrell <sparticlesteve@gmail.com>
2021-08-20 11:26:54 -0700

    latest for tf 2.5

--- build.sh
+++ build.sh
@@ -4,16 +4,17 @@
 source setup.sh
 
 # Create the conda environment
-conda create -c nvidia -n tf-2.4.2 -y \
-    python=3.8 ipython cmake
-
-#pysocks pytest #pandas #cudatoolkit=11.0 cudnn=8.0.4 nccl nvcc_linux-64
+conda create -c nvidia -n tf-2.5.0 -y \
+    python=3.9 ipython cmake pysocks pytest \
+    cudatoolkit=11.2 nccl nvcc_linux-64
+#pandas
+#cudatoolkit=11.0 cudnn=8.0.4 nccl nvcc_linux-64
 
 # Activate the conda environment
-conda activate tf-2.4.2
+conda activate tf-2.5.0
 
 # Install TensorFlow and other pip dependencies
-pip install tensorflow==2.4.2 mock
+pip install tensorflow==2.5.0 mock
 
 # Install Horovod
 ./build_horovod.sh 2>&1 | tee horovod.log
--- setup.sh
+++ setup.sh
@@ -1,14 +1,13 @@
 # Source me
-
 module load PrgEnv-gnu
-module load gcc/9.3.0
-
-# CUDA libraries
-module load cudatoolkit/20.9_11.0
-export CUDA_VERSION=11.0
-source /pscratch/sd/s/sfarrell/software/cudnn_8.0.5.39_cuda11.0_x86_64/setup.sh
+#module load cudatoolkit/20.9_11.0
+#module load craype-accel-nvidia80
+#module load nccl/2.9.8
 #module load cudnn/8.2.0
-module load nccl/2.9.6
+#module load nccl/2.9.6
+#module load mpich/3.4.1
+
+#export CUDNN_DIR=/global/common/software/nersc/cos1.3/cudnn/8.2.0/cuda/11.3
 
 export CRAYPE_LINK_TYPE=dynamic
 export CC=$(which cc)
